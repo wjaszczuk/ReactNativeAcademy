c1f5c1f97c7c55313b8f20ce2eb1fd7daaa90e21
Wiktor Jaszczuk <wjaszczuk@spyro-soft.com>
2021-04-12 21:51:38 +0200

    counter

--- App.tsx
+++ App.tsx
@@ -1,17 +1,69 @@
-import React from 'react';
-import {SafeAreaView, ScrollView, StyleSheet} from 'react-native';
+import React, {useReducer, useCallback} from 'react';
+import {SafeAreaView, ScrollView, StyleSheet, Button, Text} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const STATE_KEY = 'state';
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, {value: 0});
+
+  const handleIncrease = useCallback(() => {
+    dispatch({type: 'increase'});
+  }, [dispatch]);
+
+  const handleDecrease = useCallback(() => {
+    dispatch({type: 'decrease'});
+  }, [dispatch]);
+
+  const handleSave = useCallback(() => {
+    AsyncStorage.setItem(STATE_KEY, JSON.stringify(state));
+  }, [state]);
+
+  const handleLoad = useCallback(() => {
+    AsyncStorage.getItem(STATE_KEY).then(result => {
+      dispatch({
+        type: 'set',
+        state: result ? (JSON.parse(result) as {value: number}) : null,
+      });
+    });
+  }, [dispatch]);
+
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView style={styles.container}></ScrollView>
+      <ScrollView style={styles.container}>
+        <Text>{`Current value: ${JSON.stringify(state)}`}</Text>
+        <Button title={'+'} onPress={handleIncrease} />
+        <Button title={'-'} onPress={handleDecrease} />
+        <Button title={'save'} onPress={handleSave} />
+        <Button title={'load'} onPress={handleLoad} />
+      </ScrollView>
     </SafeAreaView>
   );
 };
 
+function reducer(
+  state: {value: number},
+  action:
+    | {type: 'increase'}
+    | {type: 'decrease'}
+    | {type: 'set'; state: {value: number} | null},
+) {
+  switch (action.type) {
+    case 'increase':
+      return {value: Number(state.value ?? 0) + 1};
+    case 'decrease':
+      return {value: Number(state.value ?? 0) - 1};
+    case 'set':
+      return action.state ? action.state : {value: 0};
+    default:
+      return state;
+  }
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'white',
   },
 });
 
